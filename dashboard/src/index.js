4483fe8619b7fdf2309b15970405142d80523b75
Ajitesh <Ajitesh72>
2024-12-04 15:34:58 +0530

    404 added

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,13 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,Router,Routes,Route } from "react-router-dom";
+import NotFoundPage from "./components/NotFoundPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+    
       <App />
     </BrowserRouter>
   </React.StrictMode>
+
 );
